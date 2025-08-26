c252b00a7c68aad4bfa8c8098799060b21b99b53
mbak-uun <sun.hidayaturrohmah@gmail.com>
2025-08-27 05:29:29 +0700

    Delete sniper directory

--- sniper/js/cex/binance.js
+++
@@ -1,6 +0,0 @@
-window.CEX_MODULES = window.CEX_MODULES || {};
-
-window.CEX_MODULES.BINANCE = {
-    url: coins => `https://api.binance.me/api/v3/depth?limit=4&symbol=${coins.symbol}USDT`,
-    processData: data => processOrderBook(data)
-};
--- sniper/js/state.js
+++
@@ -1,9 +0,0 @@
-// Defines the central state object for the application.
-
-window.APP_STATE = {
-    isMonitoring: false,
-    monitoringInterval: null,
-    tokens: [], // This will be loaded from localStorage at startup
-    settings: {}, // This will also be loaded
-    // other dynamic state can be added here
-};
